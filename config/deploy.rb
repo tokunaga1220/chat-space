8a1d899918ad273155dae61cd60fe34ff2518618
tokunaga1220 <arigatou1220ee050118@yahoo.co.jp.you@example.com>
2020-05-08 14:46:21 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/ChatSpace.pem>'] 
+                  keys: ['~/.ssh/ChatSpace.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -23,6 +23,9 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 # Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+
+set :linked_files, %w{ config/secrets.yml }
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
